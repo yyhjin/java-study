13ca5d9e342531d0495759145e7476dcc0ec21f6
yyhjin <wendy0301@naver.com>
2023-08-16 15:55:43 +0900

    io 2

---
+++ chapter04/src/main/java/io/BufferedReaderTest.java
@@ -0,0 +1,40 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest {
+
+	public static void main(String[] args) {
+		BufferedReader br = null;
+		
+		try {
+			// 기반 스트림
+			FileReader fr = new FileReader("./src/main/java/io/FileReaderTest.java");
+		
+			// 보조 스트림
+			br = new BufferedReader(fr);
+			
+			String line = null;
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found: " + e);
+		} catch (IOException e) {
+			System.out.println("error: " + e);
+		} finally {
+			try {
+				if(br != null) {
+					br.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}

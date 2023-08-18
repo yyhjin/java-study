13aff1c192c0e7aefdd983afa2f393bf3c45f96b
yyhjin <wendy0301@naver.com>
2023-08-18 17:44:38 +0900

    WebServer #1

---
+++ network/src/main/java/httpd/SimpleHttpServer.java
@@ -0,0 +1,50 @@
+package httpd;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleHttpServer {
+	private static final int PORT = 8088;
+
+	public static void main(String[] args) {
+
+		ServerSocket serverSocket = null;
+
+		try {
+			// 1. Create Server Socket
+			serverSocket = new ServerSocket();
+			   
+			// 2. Bind
+			String localhost = InetAddress.getLocalHost().getHostAddress();
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT));
+			log("bind " + localhost + ":" + PORT);
+
+			while (true) {
+				// 3. Wait for connecting ( accept )
+				Socket socket = serverSocket.accept();
+
+				// 4. Delegate Processing Request
+				new RequestHandler(socket).start();
+			}
+
+		} catch (IOException ex) {
+			log("error:" + ex);
+		} finally {
+			// 5. 자원정리
+			try {
+				if (serverSocket != null && serverSocket.isClosed() == false) {
+					serverSocket.close();
+				}
+			} catch (IOException ex) {
+				log("error:" + ex);
+			}
+		}
+	}
+
+	public static void log(String message) {
+		System.out.println("[HttpServer#" + Thread.currentThread().getId()  + "] " + message);
+	}
+}

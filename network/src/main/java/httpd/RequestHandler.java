069768fe2a5285800f2fce8507e29a7075689393
yyhjin <wendy0301@naver.com>
2023-08-21 09:52:07 +0900

    httpd #2

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -17,6 +17,10 @@ public class RequestHandler extends Thread {
 	@Override
 	public void run() {
 		try {
+			// logging Remote Host IP Address & Port
+			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
+			log( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
+			
 			// get IOStream
 			OutputStream outputStream = socket.getOutputStream();
 			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
@@ -41,15 +45,22 @@ public class RequestHandler extends Thread {
 					break;
 				}
 
-				log(line);
+//				log(line);
 			}
 
 			log(request);
 			
-			// logging Remote Host IP Address & Port
-			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
-			log( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
-					
+			
+			String[] tokens = request.split(" ");
+			if("GET".equals(tokens[0])) {
+				responseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else {
+				// methods : POST, PUT, DELETE, HEAD, CONNECT
+				// SimpleHttpServer에서는 무시(400 Bad Reqeust)
+				
+//				responseStatic400Error(outputStream, tokens[2]);
+			}
+			
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
 			outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
@@ -70,6 +81,11 @@ public class RequestHandler extends Thread {
 				log( "error:" + ex );
 			}
 		}			
+	}
+
+	private void responseStaticResource(OutputStream outputStream, String url, String protocol) {
+		
+		
 	}
 
 	public void log( String message ) {

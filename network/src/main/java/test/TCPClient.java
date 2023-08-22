f97d83452ca063491c2633001a4f4a2620a14cfc
yyhjin <wendy0301@naver.com>
2023-08-22 10:35:31 +0900

    TCP algorithm & socket options

--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 
 public class TCPClient {
 
@@ -18,6 +19,24 @@ public class TCPClient {
 		try {
 			// 1. 소켓 생성
 			socket = new Socket();
+			
+			// 1-1. 소켓 버퍼 사이즈 확인
+			int rcvBufferSize = socket.getReceiveBufferSize();
+			int sndBufferSize = socket.getSendBufferSize();
+			
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);			
+			
+			// 1-2. 소켓 버퍼사이즈 변경
+			socket.setReceiveBufferSize(1024*10);
+			socket.setSendBufferSize(1024*24);
+			
+			System.out.println(rcvBufferSize + ":" + sndBufferSize);
+			
+			// 1-3. SO_NODELAY(Nagle Algorithm off)
+			socket.setTcpNoDelay(true);
+			
+			// 1-4. SO_TIMEOUT
+			socket.setSoTimeout(3000);
 			
 			// 2. 서버 연결
 			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
@@ -42,6 +61,8 @@ public class TCPClient {
 			new String(buffer, 0, readByteCount, "utf-8");
 			System.out.println("[client] received: " + data);
 		
+		} catch (SocketTimeoutException e) {
+			System.out.println("[client] Time out!");
 		} catch (SocketException e) {
 			System.out.println("[client] suddenly closed by server");
 		} catch (IOException e) {
